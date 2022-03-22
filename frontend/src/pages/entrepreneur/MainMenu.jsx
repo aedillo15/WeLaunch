883c012e8c995325557074b4282ce51935588692
Ethan Reinke <reinkee@sheridancollege.ca>
2022-03-22 16:25:07 -0400

    Entrepreneur Menu Complete

--- frontend/src/pages/entrepreneur/MainMenu.jsx
+++ frontend/src/pages/entrepreneur/MainMenu.jsx
@@ -2,28 +2,53 @@ import React, {useState} from "react"
 import { Container, Input, Text, VStack, Box, Button, HStack, Link as UILink } from '@chakra-ui/react'
 import AnimatedPage from "../../components/AnimatedPsge"
 import { Link } from "react-router-dom"
+import Layout from "../../components/Layout";
+import Menu from "../common/CommonMenu"
 
-export const MainMenu = () => {
+
+
+
+
+export const MainMenu = (props) => {
 
     const [show, setShow] = useState(false)
 
     return(
-        <AnimatedPage> 
-            <Box w='100%' h='100%' bgGradient='linear(to-br, #4D748C, #1B3059)' >
-                <Container maxW='container.xxl' h="100%">
-                    <VStack h="100%" justifyContent="left" alignItems="left">
-                        <VStack spacing={3} w="20%" justifyContent="center" alignItems="center">
-                            <Text fontSize={35} color="#FFFFFF33">To Do</Text>
+        <Layout>
+            <AnimatedPage> 
+                <Box w='100%' h='100%' bgGradient='linear(to-br, #4D748C, #1B3059)' >
+                    <Container maxW='container.xxl' h="90vh">
                         
-                            <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Incorporate</Button>
-                            <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Product</Button>
-                            <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Cash Flow</Button>
-                            <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Sale Method</Button>
+                        <VStack h="100%" justifyContent="left" alignItems="left">
+                            <HStack>
+                            <VStack spacing={3} w="20%" justifyContent="center" alignItems="center">
+                                <Text fontSize={35} color="#FFFFFF33">The Actual Business Name</Text>
+                            
+                                <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Incorporate</Button>
+                                <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Product</Button>
+                                <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Cash Flow</Button>
+                                <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Sale Method</Button>
+                                <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >More</Button>
+                                <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Buttons</Button>
+                                <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Here</Button>
 
-                        </VStack>        
-                    </VStack>
-                </Container>            
-            </Box>  
-        </AnimatedPage> 
+                            </VStack>   
+                            <Box w="80%" pl="20">
+                                <VStack>
+                                    <Text fontSize={35} color="#F28907" align="center">Cash Flow</Text>
+
+                                        
+                                    <Button bg="#AAC1E6" color="white"  _hover={{ bg: '#89a1c7' }} w="100%" >Here</Button>
+                                    {props.children}
+                                </VStack>
+                            </Box>
+                            </HStack>
+                            
+                        </VStack>
+                        
+                    </Container>            
+                </Box>  
+            </AnimatedPage> 
+        </Layout>
     )
 }
